bc01453deff3b47777d50da576d24d07c33bc249
Monu Pal <monupalji990@gmail.com>
2026-06-03 14:32:24 +0530

    update

--- bin/app.ts
+++ bin/app.ts
@@ -197,7 +197,7 @@ app.get('/', (req: Request, res: Response) => {
  
 app.use(function (req: { headers: { origin: any; }; }, res: { setHeader: (arg0: string, arg1: string | boolean) => void; }, next: () => void) {
     var allowedOrigins = [
-        'http://localhost:3486',
+        'https://edan-frontend.onrender.com',
         'http://localhost:5173',
         'http://localhost:8092',
         'http://localhost:8080',
